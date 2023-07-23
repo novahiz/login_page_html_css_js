4c0c578c7bc22b07ee85862dbf5decbe29d95386
Tawhid Hassane <novahiz@outlook.com>
2023-07-23 15:48:40 +0100

    sythème de connexion terminé, frontend uniquement

--- src/js/login.js
+++ src/js/login.js
@@ -3,5 +3,25 @@ const forms = document.querySelector('.forms'),
       links = document.querySelectorAll('.link');
 
 pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener('click')
+    eyeIcon.addEventListener('click', () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+        console.log(pwFields)
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent from submittint
+        forms.classList.toggle("show__signup")
+    })
 })
