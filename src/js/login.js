1bec9b3e489bdcb561c6cffd3cb5e2bd68f4738a
Tawhid Hassane <novahiz@outlook.com>
2023-07-23 13:48:13 +0100

    mise en ligne

---
+++ src/js/login.js
@@ -0,0 +1,7 @@
+const forms = document.querySelector('.forms'),
+      pwShowHide = document.querySelectorAll('.eye__icon'),
+      links = document.querySelectorAll('.link');
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener('click')
+})
